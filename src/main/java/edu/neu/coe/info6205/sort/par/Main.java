6359a7bc677bd0110479e69a085b49ba9bc2ac68
Fayesun12 <114794106+Fayesun12@users.noreply.github.com>
2023-02-18 10:00:42 -0500

    Assignment5

--- src/main/java/edu/neu/coe/info6205/sort/par/Main.java
+++ src/main/java/edu/neu/coe/info6205/sort/par/Main.java
@@ -17,28 +17,69 @@ import java.util.concurrent.ForkJoinPool;
 public class Main {
 
     public static void main(String[] args) {
+
+        // Declaring no. of threads
+//        int thread= ;
+
         processArgs(args);
+//        ForkJoinPool fs=new ForkJoinPool(thread);
+
         System.out.println("Degree of parallelism: " + ForkJoinPool.getCommonPoolParallelism());
         Random random = new Random();
-        int[] array = new int[2000000];
+
+        int[] array = new int[10000000];
         ArrayList<Long> timeList = new ArrayList<>();
-        for (int j = 50; j < 100; j++) {
-            ParSort.cutoff = 10000 * (j + 1);
-            // for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
-            long time;
-            long startTime = System.currentTimeMillis();
-            for (int t = 0; t < 10; t++) {
-                for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
-                ParSort.sort(array, 0, array.length);
+        for( int k = 1; k <= 32; k = k*2) {
+            ParSort.threadCount = k;
+            ParSort.threadFJP = new ForkJoinPool(ParSort.threadCount);
+            String seriesName = "" + k + " Threads";
+//            XYSeries timeSeries = new XYSeries(seriesName);
+            double min = 99999;
+            int minCutoff = 0;
+            double avg = 0;
+            for (int j = 50; j < 100; j++) {
+
+                ParSort.cutoff = 10000 * (j + 1);
+                // for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
+                long time;
+                long startTime = System.currentTimeMillis();
+                for (int t = 0; t < 20; t++) {
+                    for (int i = 0; i < array.length; i++)
+                        array[i] = random.nextInt(10000000);
+                    ParSort.sort(array, 0, array.length);
+                }
+                long endTime = System.currentTimeMillis();
+                time = (endTime - startTime);
+                avg += time/10;
+                if(time/10 < min){
+                    minCutoff = 10000 * (j + 1);
+                    min = time/10;
+                }
+                timeList.add(time);
+//                timeSeries.add(10000 * (j + 1), time/20);
+                System.out.println("cutoff：" + (ParSort.cutoff) + "\t\t10 Times Time:" + time + "ms");
+
             }
-            long endTime = System.currentTimeMillis();
-            time = (endTime - startTime);
-            timeList.add(time);
-
-
-            System.out.println("cutoff：" + (ParSort.cutoff) + "\t\t10times Time:" + time + "ms");
-
+            System.out.println("For threads " + k + " min is = " + min + " at cutoff " + minCutoff + "ms and average is = " + avg/50+"ms");
         }
+
+//        for (int j = 50; j < 100; j++) {
+//            ParSort.cutoff = 10000 * (j + 1);
+//            // for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
+//            long time;
+//            long startTime = System.currentTimeMillis();
+//            for (int t = 0; t < 10; t++) {
+//                for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
+//                ParSort.sort(array, 0, array.length);
+//            }
+//            long endTime = System.currentTimeMillis();
+//            time = (endTime - startTime);
+//            timeList.add(time);
+//
+//
+//            System.out.println("cutoff：" + (ParSort.cutoff) + "\t\t10times Time:" + time + "ms");
+//
+//        }
         try {
             FileOutputStream fis = new FileOutputStream("./src/result.csv");
             OutputStreamWriter isr = new OutputStreamWriter(fis);
@@ -73,7 +114,7 @@ public class Main {
     private static void processCommand(String x, String y) {
         if (x.equalsIgnoreCase("N")) setConfig(x, Integer.parseInt(y));
         else
-            // TODO sort this out
+            // CONSIDER sort this out
             if (x.equalsIgnoreCase("P")) //noinspection ResultOfMethodCallIgnored
                 ForkJoinPool.getCommonPoolParallelism();
     }
